e697e2c30bcc2fb305af9e71035efd5b0d5ce46d
Ivan <guryanoviv@gmail.com>
2021-01-15 00:30:38 +0300

    Test commit

--- src/physics/physics/Collider.java
+++ src/physics/physics/Collider.java
@@ -3,7 +3,6 @@ package physics.physics;
 import physics.geometry.IntersectionalPair;
 import physics.geometry.SphereIntersection;
 import physics.geometry.SphereToLineIntersection;
-import physics.geometry.Vector2;
 import physics.sphere.ASS;
 import physics.polygons.PhysicalPolygon;
 
